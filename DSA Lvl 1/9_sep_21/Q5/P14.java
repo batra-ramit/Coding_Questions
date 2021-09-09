d028e4071be39e0bca24ab6b1bd7a9ead4d49abe
ramit batra <ramitbatra.official@gmail.com>
2021-09-09 23:58:51 +0530

    completed question 3&5 9 sep

---
+++ DSA Lvl 1/9_sep_21/Q5/P14.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class P14 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+       int x=scn.nextInt();
+       for(int i=1;i<=10;i++)
+           System.out.println(x+" * "+i+" = "+i*x);
+       
+
+    }
+}
