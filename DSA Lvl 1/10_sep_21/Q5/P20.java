6082966241b38b3a2ce1bdc4015b1f4875f3ca5f
ramit batra <ramitbatra.official@gmail.com>
2021-09-10 23:03:39 +0530

    completed question 5&6 10_sep

---
+++ DSA Lvl 1/10_sep_21/Q5/P20.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class P20 {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+
+int n=scn.nextInt();
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=n;j++){
+        if( (j==1) || (j==n) || (i>n/2 && i==j) || (i>n/2 && i+j==n+1) )
+        System.out.print("*\t");
+        else
+        System.out.print("\t");
+    }System.out.println();
+    
+}
+
+
+  }
+}
