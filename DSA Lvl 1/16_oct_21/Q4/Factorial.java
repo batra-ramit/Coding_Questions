67ec5aa9850e85dbf31bce54e5893c252d02f71a
ramit batra <ramitbatra.official@gmail.com>
2021-10-17 17:57:51 +0530

    Recursion basic ques

---
+++ DSA Lvl 1/16_oct_21/Q4/Factorial.java
@@ -0,0 +1,23 @@
+
+
+  
+    import java.io.*;
+    import java.util.*;
+    
+    public class Factorial {
+    
+    
+        public static void main(String[] args) throws Exception {
+           Scanner scn=new Scanner(System.in);
+           int n=scn.nextInt();
+           System.out.println(factorial(n));
+        }
+    
+        public static int factorial(int n){
+            if(n==1)
+            return 1;
+            return n*factorial(n-1);
+        
+        }
+    
+    }
