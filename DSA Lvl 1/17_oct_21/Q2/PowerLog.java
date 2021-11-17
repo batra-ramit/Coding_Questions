e8b6a13a8720b9f04a12a8f9d063521588b37f2d
ramit batra <ramitbatra.official@gmail.com>
2021-11-17 22:24:30 +0530

    added merge sort

--- DSA Lvl 1/17_oct_21/Q2/PowerLog.java
+++ DSA Lvl 1/17_oct_21/Q2/PowerLog.java
@@ -1,24 +1,20 @@
+import java.io.*;
+import java.util.*;
 
-    import java.io.*;
-    import java.util.*;
-    public class PowerLog {
-    
-    
-    
-        public static void main(String[] args) throws Exception {
-            Scanner sc=new Scanner(System.in);
-            int x=sc.nextInt();
-            int n=sc.nextInt();
-            System.out.println(power(x,n));
-        }
-    
-        public static int power(int x, int n){
-            if(n==0)
-            return 1;
-           int p1= power(x,n/2)*power(x,n/2);
-            if(n%2==1)
-            p1=p1*x;
-            return p1;
-        }
-    
-    }
+public class PowerLog {
+
+  public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    int n = sc.nextInt();
+    System.out.println(power(x, n));
+  }
+
+  public static int power(int x, int n) {
+    if (n == 0) return 1;
+    int p = power(x, n / 2);
+    int p1 = p * p;
+    if (n % 2 == 1) p1 = p1 * x;
+    return p1;
+  }
+}
