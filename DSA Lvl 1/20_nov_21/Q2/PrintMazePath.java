e65b0a24064e33333cd42e4952a8b4cb56fe9b04
ramit batra <ramitbatra.official@gmail.com>
2021-11-20 23:50:03 +0530

    added print maze path

--- DSA Lvl 1/20_nov_21/Q2/PrintMazePath.java
+++ DSA Lvl 1/20_nov_21/Q2/PrintMazePath.java
@@ -1,3 +1,29 @@
-public class PrintMazePath {
-    
+import java.io.*;
+	import java.util.*;
+
+	public class PrintMazePath {
+
+	    public static void main(String[] args) throws Exception {
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int m=sc.nextInt();
+
+printMazePaths(1,1,n,m,"");
+	    }
+
+	   
+	    public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
+	        
+
+if(sc==dc && sr==dr){
+    System.out.println(psf);
+    return;
+}else if(sr>dr || sc>dc){
+    return;
 }
+
+            printMazePaths(sr,sc+1,dr,dc,psf+"h");
+            printMazePaths(sr+1,sc,dr,dc,psf+"v");
+	    }
+
+	}
