07a4b05d5c123a9ce78507f4876e4233f13e37de
ramit batra <ramitbatra.official@gmail.com>
2021-12-19 17:28:11 +0530

    added PivotInSortedRotatedArr

---
+++ DSA Lvl 1/19_dec_21/Q1/PivotInSortedRotatedArr.java
@@ -0,0 +1,31 @@
+package Q1;
+import java.io.*;
+import java.util.*;
+
+public class PivotInSortedRotatedArr {
+
+  public static int findPivot(int[] arr) {
+    //    Arrays.sort(arr);
+    //    return arr[0];
+
+    int lo = 0;
+    int hi = arr.length - 1;
+
+    while (lo < hi) {
+      int mid = (lo + hi) / 2;
+      if (arr[mid] < arr[hi]) hi = mid; else lo = mid + 1;
+    }
+    return arr[lo];
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    int pivot = findPivot(arr);
+    System.out.println(pivot);
+  }
+}
