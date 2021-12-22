85d86b8b81052d58110bae0093942f70186c810e
ramit batra <ramitbatra.official@gmail.com>
2021-12-22 20:28:00 +0530

    added Basic JS

--- Dev/Basic JS/21_dec_21/Q1.js
+++ Dev/Basic JS/21_dec_21/Q1.js
@@ -1,15 +1,32 @@
+
+// Description:
+// Find output of the following:
+
 function f() {
-    console.log(arguments);
+  console.log(arguments);
 }
 
 function f(a, b) {
-    return a + b;
+  return a + b;
 }
 
 console.log(f(2, 3, 4, 5));
 
-function f(a, b, c, d) {
-    return a + b + c + d;
+function f(x, y, z, t) {
+    return x + y + z + t;
 }
 
 console.log(f(2, 3, 4, 5));
+
+// Q2
+
+
+function globalFunction(x) {
+    return function outerFunction(y) {
+      return function innerFunction(z) {
+        return x + y + z;
+      };
+    };
+}
+  
+console.log(globalFunction(5));
