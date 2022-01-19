a8e849825becffe6620c163ebda0d31d3264be5c
ramit batra <ramitbatra.official@gmail.com>
2022-01-19 22:10:10 +0530

    added Recursion Back Tracking TargetSumSubset

--- DSA Lvl 1/Practice/Main.java
+++ DSA Lvl 1/Practice/Main.java
@@ -2,116 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-  public static class Node {
-    int data;
-    Node left;
-    Node right;
 
-    Node(int data, Node left, Node right) {
-      this.data = data;
-      this.left = left;
-      this.right = right;
-    }
-  }
-
-  public static class Pair {
-    Node node;
-    int state;
-
-    Pair(Node node, int state) {
-      this.node = node;
-      this.state = state;
-    }
-  }
-
-  public static Node construct(Integer[] arr) {
-    Node root = new Node(arr[0], null, null);
-    Pair rtp = new Pair(root, 1);
-
-    Stack<Pair> st = new Stack<>();
-    st.push(rtp);
-
-    int idx = 0;
-    while (st.size() > 0) {
-      Pair top = st.peek();
-      if (top.state == 1) {
-        idx++;
-        if (arr[idx] != null) {
-          top.node.left = new Node(arr[idx], null, null);
-          Pair lp = new Pair(top.node.left, 1);
-          st.push(lp);
-        } else {
-          top.node.left = null;
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int[][] arr = new int[n][m];
+        int [][]temp=new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = scn.nextInt();
+            }
         }
 
-        top.state++;
-      } else if (top.state == 2) {
-        idx++;
-        if (arr[idx] != null) {
-          top.node.right = new Node(arr[idx], null, null);
-          Pair rp = new Pair(top.node.right, 1);
-          st.push(rp);
-        } else {
-          top.node.right = null;
-        }
-
-        top.state++;
-      } else {
-        st.pop();
-      }
+        floodfill(arr,temp, 0, 0,"");
     }
+    
+   
+    public static void floodfill(int[][] maze,int[][] temp, int sr, int sc, String asf) {
+    
+    if(maze[sr][sc]==1 || temp[sr][sc]==1 || sr>maze.length-1 || sc>maze[0].length-1 || sr<0 || sc<0)
+    return;
 
-    return root;
-  }
+    if(sc==maze[0].length-1 && sr==maze.length-1)
+    {System.out.println(asf);
+    return;}
 
-  public static void display(Node node) {
-    if (node == null) {
-      return;
+temp[sr][sc]=1;
+floodfill(maze,temp,sr+1,sc,asf+"d");
+floodfill(maze,temp,sr-1,sc,asf+"t");
+floodfill(maze,temp,sr,sc+1,asf+"r");
+floodfill(maze,temp,sr,sc-1,asf+"l");
+temp[sr][sc]=0;
     }
-
-    String str = "";
-    str += node.left == null ? "." : node.left.data + "";
-    str += " <- " + node.data + " -> ";
-    str += node.right == null ? "." : node.right.data + "";
-    System.out.println(str);
-
-    display(node.left);
-    display(node.right);
-  }
-
-  public static Node removeLeaves(Node node){
-    if(node==null)
-    return null;
-
-    Node l=removeLeaves(node.left);
-    Node r=removeLeaves(node.right);
-
-
-
-    if (node.left == null && node.right == null) {
-        node = null;
-    } else {
-        
-    }
-    return node;
-  }
-
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine());
-    Integer[] arr = new Integer[n];
-    String[] values = br.readLine().split(" ");
-    for (int i = 0; i < n; i++) {
-      if (values[i].equals("n") == false) {
-        arr[i] = Integer.parseInt(values[i]);
-      } else {
-        arr[i] = null;
-      }
-    }
-
-    Node root = construct(arr);
-    root = removeLeaves(root);
-    display(root);
-  }
-
 }
