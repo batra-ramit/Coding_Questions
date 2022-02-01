8e5785b7f4cb77d417514b35a4c1a63985045cf1
ramit batra <ramitbatra.official@gmail.com>
2022-02-01 23:17:30 +0530

    added Hashmap Longest Seq

--- DSA Lvl 1/Practice/Main.java
+++ DSA Lvl 1/Practice/Main.java
@@ -1,33 +1,109 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
 
-public static void main(String[] args) throws Exception {
-    Scanner sc=new Scanner(System.in);
-    int n=sc.nextInt();
-    int max=-1;
-    int maxSeq=-1;
-    HashMap<Integer,Integer> map=new HashMap<>();
-    for(int i=0;i<n;i++){
-        int a=sc.nextInt();
-        if (map.containsKey(a) == false) {
-          int b = a--;
-            if(map.containsKey(b)==false)
-           map.put(a,1);
-           else{
-               Integer freq=map.get(b);
-               map.put(a,++freq);
-               if(freq>max)
-               {max=freq;
-               maxSeq=a;
-           }}
-        }
+  public static class PriorityQueue {
+
+    ArrayList<Integer> data;
+
+    public PriorityQueue() {
+      data = new ArrayList<>();
     }
 
+    public void add(int val) {
+      data.add(val);
 
-    for(int j=maxSeq-max;j<=maxSeq;j++)
-    System.out.println(j);
- }
+      int i = data.size() - 1;
 
+      while (i >= 0) {
+        int parentindex = i / 2;
+        int parent = data.get(parentindex);
+        int child = data.get(i);
+        if (child < parent) {
+          data.set(parentindex, child);
+          data.set(i, parent);
+        } else {
+          break;
+        }
+        i = parentindex;
+      }
+    }
+
+    public int remove() {
+      if (data.size() == 0) {
+        System.out.println("Underflow");
+        return -1;
+      }
+
+      int size = data.size() - 1;
+      int value = data.get(size);
+      int temp = data.get(0);
+
+      data.set(0, value);
+      data.set(size, temp);
+
+      data.remove(size);
+      int i = 0;
+      while (i < data.size()) {
+        int min = i;
+
+        int li = i * 2 + 1;
+        int ri = i * 2 + 2;
+
+        if (li < data.size() && data.get(li) < data.get(min)) min = li;
+        if (ri < data.size() && data.get(ri) < data.get(min)) min = ri;
+
+        if (min != i) {
+          int parent = data.get(i);
+          int child = data.get(min);
+          data.set(i, child);
+          data.set(min, parent);
+          i = min;
+        } else {
+          break;
+        }
+      }
+
+      return temp;
+    }
+
+    public int peek() {
+      if (data.size() == 0) {
+        System.out.println("Underflow");
+        return -1;
+      }
+      return data.get(0);
+    }
+
+    public int size() {
+      return data.size();
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    PriorityQueue qu = new PriorityQueue();
+
+    String str = br.readLine();
+    while (str.equals("quit") == false) {
+      if (str.startsWith("add")) {
+        int val = Integer.parseInt(str.split(" ")[1]);
+        qu.add(val);
+      } else if (str.startsWith("remove")) {
+        int val = qu.remove();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("peek")) {
+        int val = qu.peek();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("size")) {
+        System.out.println(qu.size());
+      }
+      str = br.readLine();
+    }
+  }
 }
