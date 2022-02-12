2358fc059004efad90942de211602bca6cfc2c70
ramit batra <ramitbatra.official@gmail.com>
2022-02-12 22:22:14 +0530

    changed practice

--- DSA Lvl 1/Practice/Main.java
+++ DSA Lvl 1/Practice/Main.java
@@ -2,93 +2,97 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-   static class Edge {
-      int src;
-      int nbr;
-      int wt;
 
-      Edge(int src, int nbr, int wt) {
-         this.src = src;
-         this.nbr = nbr;
-         this.wt = wt;
-      }
-   }
+  static class Edge {
 
-   static class Pair{
-       int v;
-       int level;
+    int src;
+    int nbr;
+    int wt;
 
-       Pair(int v,int level){
-           this.v=v;
-           this.level=level;
+    Edge(int src, int nbr, int wt) {
+      this.src = src;
+      this.nbr = nbr;
+      this.wt = wt;
+    }
+  }
 
-       }
-   }
+  static class Pair {
 
-   public static void main(String[] args) throws Exception {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int v;
+    int level;
 
-      int vtces = Integer.parseInt(br.readLine());
-      ArrayList<Edge>[] graph = new ArrayList[vtces];
-      for (int i = 0; i < vtces; i++) {
-         graph[i] = new ArrayList<>();
-      }
+    Pair(int v, int level) {
+      this.v = v;
+      this.level = level;
+    }
+  }
 
-      int edges = Integer.parseInt(br.readLine());
-      for (int i = 0; i < edges; i++) {
-         String[] parts = br.readLine().split(" ");
-         int v1 = Integer.parseInt(parts[0]);
-         int v2 = Integer.parseInt(parts[1]);
-         int wt = Integer.parseInt(parts[2]);
-         graph[v1].add(new Edge(v1, v2, wt));
-         graph[v2].add(new Edge(v2, v1, wt));
-      }
-int[] visited=new int[vtces];
-Arrays.fill(visited,-1);
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-      for(int i=0;i<vtces;i++){
-         if(visited[i]==-1)
-       {boolean boool=IsGraphBipartite(graph,visited,i);
-         if(boool==false){
-             System.out.println(boool);
+    int vtces = Integer.parseInt(br.readLine());
+    ArrayList<Edge>[] graph = new ArrayList[vtces];
+    for (int i = 0; i < vtces; i++) {
+      graph[i] = new ArrayList<>();
+    }
+
+    int edges = Integer.parseInt(br.readLine());
+    for (int i = 0; i < edges; i++) {
+      String[] parts = br.readLine().split(" ");
+      int v1 = Integer.parseInt(parts[0]);
+      int v2 = Integer.parseInt(parts[1]);
+      int wt = Integer.parseInt(parts[2]);
+      graph[v1].add(new Edge(v1, v2, wt));
+      graph[v2].add(new Edge(v2, v1, wt));
+    }
+
+    int[] visited = new int[vtces];
+    int level = 0;
+
+    Arrays.fill(visited, -1);
+    boolean bool = true;
+    for (int i = 0; i < vtces; i++) {
+       if (visited[i] == -1)
+       {
+          bool = Isbipartite(graph, visited, level, i);
+          if (bool == false) {
+             System.out.print(false);
              return;
-         }
-         
-         }
-         }
-   }
-
-   public static boolean IsGraphBipartite(ArrayList<Edge>[] graph,int[] visited,int src){
-
-Queue<Pair> queue=new ArrayDeque<>();
-
-Pair pp=new Pair(src,0);
-
-queue.add(pp);
-
-while(queue.size()>0){
-
-Pair temp=queue.remove();
-int nsr=temp.v;
-if(visited[nsr]!=-1){
-if(temp.level!=visited[nsr])
-return false;
-   }else{
-visited[nsr]=temp.level;
-       for(int i=0;i<graph[nsr].size();i++){
-           Edge e=graph[nsr].get(i); 
-           if(visited[e.nbr]==-1){
-              Pair cp = new Pair(e.nbr, temp.level + 1);
-               queue.add(cp);
-           }
+          }
        }
-       
-       }
-   }
+    }
 
-   return true;
-   }
+    System.out.print(true);
+  }
 
+  public static boolean Isbipartite(
+    ArrayList<Edge>[] graph,
+    int visited[],
+    int level,
+    int src
+  ) {
+    Queue<Pair> queue = new ArrayDeque<>();
+    Pair pp = new Pair(src, 0);
+    queue.add(pp);
 
-   
+    while (queue.size() > 0) {
+      Pair temp = queue.remove();
+
+      if (visited[temp.v] != -1) {
+        if (temp.level != visited[temp.v]) return false;
+     } else {
+        visited[temp.v] = temp.level;
+        for (int i = 0; i < graph[temp.v].size(); i++) {
+          Edge e = graph[temp.v].get(i);
+
+          if (visited[e.nbr] == -1) {
+            Pair cp = new Pair(e.nbr, temp.level + 1);
+            queue.add(cp);
+          }
+        }
+      }
+    }
+
+    return true;
+  }
 }
