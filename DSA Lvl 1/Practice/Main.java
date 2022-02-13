e74c9594acce0db6f2f518811dcb210f021c1133
ramit batra <ramitbatra.official@gmail.com>
2022-02-13 23:51:10 +0530

    practice

--- DSA Lvl 1/Practice/Main.java
+++ DSA Lvl 1/Practice/Main.java
@@ -3,96 +3,47 @@ import java.util.*;
 
 public class Main {
 
-  static class Edge {
+  public static void display(int[] a) {
+    StringBuilder sb = new StringBuilder();
 
-    int src;
-    int nbr;
-    int wt;
-
-    Edge(int src, int nbr, int wt) {
-      this.src = src;
-      this.nbr = nbr;
-      this.wt = wt;
+    for (int val : a) {
+      sb.append(val + "\n");
     }
-  }
-
-  static class Pair {
-
-    int v;
-    int level;
-
-    Pair(int v, int level) {
-      this.v = v;
-      this.level = level;
-    }
+    System.out.println(sb);
   }
 
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    int vtces = Integer.parseInt(br.readLine());
-    ArrayList<Edge>[] graph = new ArrayList[vtces];
-    for (int i = 0; i < vtces; i++) {
-      graph[i] = new ArrayList<>();
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for (int i = 0; i < n; i++) {
+      a[i] = Integer.parseInt(br.readLine());
     }
 
-    int edges = Integer.parseInt(br.readLine());
-    for (int i = 0; i < edges; i++) {
-      String[] parts = br.readLine().split(" ");
-      int v1 = Integer.parseInt(parts[0]);
-      int v2 = Integer.parseInt(parts[1]);
-      int wt = Integer.parseInt(parts[2]);
-      graph[v1].add(new Edge(v1, v2, wt));
-      graph[v2].add(new Edge(v2, v1, wt));
-    }
-
-    int[] visited = new int[vtces];
-    int level = 0;
-
-    Arrays.fill(visited, -1);
-    boolean bool = true;
-    for (int i = 0; i < vtces; i++) {
-       if (visited[i] == -1)
-       {
-          bool = Isbipartite(graph, visited, level, i);
-          if (bool == false) {
-             System.out.print(false);
-             return;
-          }
-       }
-    }
-
-    System.out.print(true);
+    int[] nge = solve(a);
+    display(nge);
   }
 
-  public static boolean Isbipartite(
-    ArrayList<Edge>[] graph,
-    int visited[],
-    int level,
-    int src
-  ) {
-    Queue<Pair> queue = new ArrayDeque<>();
-    Pair pp = new Pair(src, 0);
-    queue.add(pp);
+  public static int[] solve(int[] arr) {
+    Stack<Integer> st = new Stack<>();
+    int[] ans = new int[arr.length];
 
-    while (queue.size() > 0) {
-      Pair temp = queue.remove();
+    st.add(0);
+    
+    for (int i = 1; i < arr.length; i++) {
+      while (st.size() > 0 && arr[i] >= arr[st.peek()]) {
+        int pidex = st.pop();
+        ans[pidex] = arr[i];
 
-      if (visited[temp.v] != -1) {
-        if (temp.level != visited[temp.v]) return false;
-     } else {
-        visited[temp.v] = temp.level;
-        for (int i = 0; i < graph[temp.v].size(); i++) {
-          Edge e = graph[temp.v].get(i);
-
-          if (visited[e.nbr] == -1) {
-            Pair cp = new Pair(e.nbr, temp.level + 1);
-            queue.add(cp);
-          }
-        }
       }
+      st.push(i);
     }
 
-    return true;
+    while (st.size() > 0) {
+      int pidex=st.pop();
+      ans[pidex]=-1;
+    }
+    return ans;
   }
 }
