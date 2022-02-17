6b96bbcf101edb2db35c9437240ce9c29bdb1236
ramit batra <ramitbatra.official@gmail.com>
2022-02-17 23:44:34 +0530

    added practice

--- DSA Lvl 1/Practice/Main.java
+++ DSA Lvl 1/Practice/Main.java
@@ -1,67 +1,45 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-  
+public class Main {
 
-public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
+    // write your code here
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String exp = br.readLine();
+    int n = Integer.parseInt(br.readLine());
+    int[][] arr = new int[n][n];
 
-Stack<Character> ostack=new Stack<>();
-Stack<Integer> vstack=new Stack<>();
-
-for(int i=0;i<exp.length();i++){
-    char ch=exp.charAt(i);
-
-    if(ch=='(')
-    ostack.push(ch);
-    else if(ch>='0' && ch<='9')
-    vstack.push(ch-'0');
-    else if(ch=='+' || ch=='-' || ch=='*' || ch=='/'){
-        while(ostack.size()>0 && ostack.peek()!='('&& precedence(ostack.peek())>=precedence(ch))
-        InfixEval(vstack,ostack);
-
-        ostack.push(ch);
+    for (int j = 0; j < n; j++) {
+      String line = br.readLine();
+      for (int k = 0; k < n; k++) {
+        arr[j][k] = line.charAt(k) - '0';
+      }
     }
-    else if(ch==')'){
-        while(ostack.size()>0 && ostack.peek()!='(')
-        InfixEval(vstack,ostack);
 
-        ostack.pop();
+    findCelebrity(arr);
+  }
+
+  public static void findCelebrity(int[][] arr) {
+    Stack<Integer> st = new Stack<>();
+
+    for (int i = 0; i < arr.length; i++) st.push(i);
+
+    while (st.size() > 1) {
+      int v1 = st.pop();
+      int v2 = st.pop();
+
+      if (arr[v1][v2] == 0) st.push(v1);
+
+      if (arr[v2][v1] == 0) st.push(v2);
     }
-}
-
-while(ostack.size()>0){
-    InfixEval(vstack,ostack);
-}
-System.out.println(vstack.peek());
-    
- }
-
- static int operation (int v1,int v2,char op){
-     if(op=='+')
-     return v1+v2;
-     else if(op=='/')
-     return v1/v2;
-     else if(op=='*')
-     return v1*v2;
-     else
-     return v1-v2;
- }
-
- static int precedence(char op){
-     if(op=='*' || op=='/')
-     return 2;
-     else
-     return 1;
- }
-
- static void InfixEval(Stack<Integer> vstack,Stack<Character> ostack){
-     char op=ostack.pop();
-     int v2=vstack.pop();
-     int v1=vstack.pop();
-     int ans=operation(v1,v2,op);
-     vstack.push(ans);
- }
+    boolean b = true;
+    int res = st.peek();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[res][i] == 1) b = false;
+      if (arr[i][res] == 0 && i != res) b = false;
+    }
+    if (b == true) System.out.println(st.peek()); else System.out.println(
+      "none"
+    );
+  }
 }
